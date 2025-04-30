33d2041f1c5858d500310ae7e1316efae01ec77b
Ayush Bajracharya <ayushbajracharya02@gmail.com>
2025-04-30 06:40:06 +0545

    removed tracking of migration folder

--- src/db/migration/0000_regular_catseye.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `company_details` (
-	`logo` text NOT NULL,
-	`email` text NOT NULL,
-	`address` text NOT NULL,
-	`contact_number` int NOT NULL
-);
--- src/db/migration/0001_certain_vermin.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `company_details` ADD `created_at` timestamp DEFAULT (now()) NOT NULL;--> statement-breakpoint
-ALTER TABLE `company_details` ADD `updated_at` timestamp;
