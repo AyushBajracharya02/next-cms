5f8dd94baf3f82b1507c40da96a404e0ae678011
Ayush Bajracharya <ayushbajracharya02@gmail.com>
2025-05-02 13:20:05 +0545

    homepage crud started

---
+++ src/app/admin/(dashboard)/dashboard/home-page/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+export default function Page() {
+    const bannerForm = useForm({
+        resolver: zodResolver(
+            z.object({
+                title: z.string(),
+                subtitle: z.string(),
+            })
+        ),
+    });
+    return (
+        <Card className="@container">
+            <CardHeader>Banner Content</CardHeader>
+            <CardContent>
+                <Form {...bannerForm}>
+                    <form onSubmit={bannerForm.handleSubmit(() => {})}>
+                        <div className="grid grid-cols-1 @3xl:grid-cols-2 @5xl:grid-cols-4 gap-4">
+                            <FormField
+                                name="title"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Title</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                name="subtitle"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Subtitle</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+                        <Button className="mt-4">Submit</Button>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    );
+}
